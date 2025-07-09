564b4fcee68ae5f5c0c81d4a7b922351f30a2257
camcoder337 <camcoder337@gmail.com>
2025-07-09 12:16:44 +0100

    feat: fictive splash screen to process service initialization

---
+++ lib/app/view/splash_page.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:yansnet/app/widgets/splash_logo.dart';
+import 'package:yansnet/core/utils/pref_utils.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async{
+      final sessionId = await PrefUtils().getAuthToken();
+      debugPrint('starting to get token from cache storage');
+
+      try{
+        //TODO (camcoder): initialize app user config throught fetchs via cubits
+        //TODO (camcoder): init timeago, rabbit and notification services
+
+
+        //TODO (camcoder): fetch user informations
+        unawaited(
+          context.pushNamed("home"),
+        );
+
+
+      } catch(e){
+        debugPrint('Some splash initiations failed');
+        unawaited(
+          context.pushNamed("home"),
+        );
+      }
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: SplashLogo(),
+      ),
+    );
+  }
+}
