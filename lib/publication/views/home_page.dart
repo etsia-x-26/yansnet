011081cefe5cf06f2f1cc9dbc65ee0f6db77dc00
Navy-sama <yvan.nouafo@2026.ucac-icam.com>
2025-07-11 07:22:20 +0100

    feat/home

---
+++ lib/publication/views/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:yansnet/publication/widgets/add_button.dart';
+import 'package:yansnet/publication/widgets/home/post_item.dart';
+import 'package:yansnet/publication/widgets/yansnet_app_bar.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final postTimestamp = DateTime(2004, 1, 29, 17, 35);
+    final postDate =
+        '${postTimestamp.day}/${postTimestamp.month}/${postTimestamp.year % 10000}';
+    final postTime =
+        '${postTimestamp.hour.toString().padLeft(2, '0')}:'
+        '${postTimestamp.minute.toString().padLeft(2, '0')}';
+
+    return Scaffold(
+      appBar: const YansnetAppBar(hasNotification: true, messageCount: 1),
+      floatingActionButton: const AddButton(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 18),
+        child: ListView(
+          children: [
+            PostItem(
+              username: 'thatjoshguy69',
+              displayName: 'Josh Design',
+              content: "I'm cookin' something cool 😎",
+              imageUrl:
+                  'https://i.pinimg.com/474x/e4/29/69/e429697f201ade295adfdc189d656047.jpg',
+              time: '$postTime - $postDate',
+              likes: 15,
+              comments: 15,
+            ),
+            const PostItem(
+              username: 'bennettbuhner',
+              displayName: 'Bennt Bruhner',
+              content: "Oooooo what's this place?",
+              imageUrl:
+                  'https://i.pinimg.com/736x/ee/97/aa/ee97aad105740376bb29f9fd130931bf.jpg',
+              time: '',
+              likes: 0,
+              comments: 0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
